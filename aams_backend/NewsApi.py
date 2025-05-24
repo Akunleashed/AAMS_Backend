e04a355454054cd30775e4f0ebbd7e35fd112c47
Akshat Kushwaha <akshat.24bcs10060@sst.scaler.com>
2025-05-24 20:48:19 +0530

    initial commit

---
+++ aams_backend/NewsApi.py
@@ -0,0 +1,32 @@
+import requests
+
+url = 'https://eventregistry.org/api/v1/article/getArticles'
+
+
+request_body = {
+  "action": "getArticles",
+  "keyword": "reliance",
+  "keywordLoc": "title",
+  "categoryUri": ["dmoz/Business/Accounting","dmoz/Business/Business and Society","dmoz/Business/Associations","dmoz/Business/Business Services","dmoz/Business/Cooperatives","dmoz/Business/Financial Services","dmoz/Business/Marketing and Advertising","dmoz/Business/Business News and Media","dmoz/Business/Small Business","dmoz/Business/Wholesale Trade"],
+  "categoryOper": "or",
+  "sourceLocationUri": [
+    "http://en.wikipedia.org/wiki/India"
+  ],
+  "ignoreSourceGroupUri": "paywall/paywalled_sources",
+  "articlesPage": 1,
+  "articlesCount": 10,
+  "articlesSortBy": "date",
+  "articlesArticleBodyLen": 10,
+  "isDuplicateFiler": "skipDuplicates",
+  "lang": "eng",
+  "dataType": [
+    "news",
+    "pr"
+  ],
+  "forceMaxDataTimeWindow": 7,
+  "resultType": "articles",
+  "apiKey": "db19b4bf-29e5-40cb-b319-4820814ea84c"
+}
+
+response = requests.get(url, request_body)
+print(response.json())
