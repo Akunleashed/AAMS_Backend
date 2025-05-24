cd1ecc04191f8db8d5edf6626e11b49200e84772
SushantBajaj <sushant1108bajaj@gmail.com>
2025-05-24 21:15:41 +0530

    added Sentiment anylysis

---
+++ aams_backend/Sentiment.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+finbert_tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+finbert_model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
+finbert_classifier = pipeline("sentiment-analysis", model=finbert_model, tokenizer=finbert_tokenizer, device=0)
+
+def get_sentiment(news: list[str]) ->int:
+    result = finbert_classifier(news)
+    total = 0
+    for i in result:
+        temp = 0
+        if(i['label']=='positive'):
+            temp = (50+(50*i['score']))
+        elif(i['label']=='neutral'):
+            temp=(40+(20*i['score']))
+        else:
+            temp=(50-(50*(i['score'])))
+        total+=temp
+    return total/len(result)
+
+
--- aams_backend/main.py
+++ aams_backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from NewsApi import getNews
+from Sentiment import get_sentiment
 app = FastAPI()
 
 @app.get("/")
